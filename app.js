283c329291c2bb005eb8446f02036699a43f29ca
venkateshwarluk <ksrinuvenkatesh@gmail.com>
2016-11-07 14:59:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+var address = {
+	city: "Bengalore",
+	state: "Karnataka",
+	pinCode: "602026"
+};
+
+var Employee = {
+	name: "Raj",
+	age: "25 Years",
+	address: address
+};
+
+console.log(Employee.name);
+console.log(Employee.age);
+console.log(Employee.address.city);
+console.log(Employee.address.state);
+console.log(Employee.address.pinCode);
